577a31156105c431db58aca8aad8b7300ed840eb
olatemitayo <olatunjitemitayo444@gmail.com>
2024-01-14 21:17:08 +0100

    latest commit

--- components/asset-price.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export  function AssetPrice() {
-  return (
-    <div>AssetPrice</div>
-  )
-}
--- components/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './trending'
-export * from './custom-table'
-export * from  './navbar'
-export * from  './trending-table-column'
-export * from './asset-price'
-export * from './asset-loading'
